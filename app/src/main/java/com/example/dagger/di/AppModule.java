5a00dd19ccb4a2b9a5093334b3f4afcd91ac8f9b
Igor Novikov <novikov.mail@inbox.ru>
2020-12-17 18:43:43 +0300

    SingleTone scope

--- app/src/main/java/com/example/dagger/di/AppModule.java
+++ app/src/main/java/com/example/dagger/di/AppModule.java
@@ -10,6 +10,8 @@ import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.dagger.R;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -17,19 +19,21 @@ import dagger.Provides;
 public class AppModule {
     //Here we put everything which wont change entire life of our application
     //Always use static methods. It's more efficient.
-
+    @Singleton
     @Provides
     static RequestOptions provideRequestOptions(){
         return RequestOptions.placeholderOf(R.drawable.white_background)
                 .error(R.drawable.white_background);
     }
 
+    @Singleton
     @Provides
     static RequestManager provideGlideInstance(Application application, RequestOptions requestOptions){
         return Glide.with(application)
                 .setDefaultRequestOptions(requestOptions);
     }
 
+    @Singleton
     @Provides
     static Drawable provideAppDrawable(Application application){
         return ContextCompat.getDrawable(application,R.drawable.logo);
