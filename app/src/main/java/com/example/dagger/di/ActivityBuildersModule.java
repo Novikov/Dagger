76f8c290cb7aa34d8bd6d28379a4e9687bae61b6
Igor Novikov <novikov.mail@inbox.ru>
2020-12-18 09:57:55 +0300

    Part 14. ActivityBuildersModule comment

--- app/src/main/java/com/example/dagger/di/ActivityBuildersModule.java
+++ app/src/main/java/com/example/dagger/di/ActivityBuildersModule.java
@@ -8,7 +8,8 @@ import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class ActivityBuildersModule {
-
+    //Here we declare subcomponent. Look at the ActivityBuildersModule_ContributeAuthActivity in the generated code
+    //It's possible create by more complicated way(Look part 14 lesson), but we use android annotations. It's more concise.
     @ContributesAndroidInjector(modules = {AuthViewModulesModule.class})
     abstract AuthActivity contributeAuthActivity();
 
