9d26c2a3d1690a463c9d32d1bdf147eb55d1c6f9
Igor Novikov <novikov.mail@inbox.ru>
2020-12-19 12:49:25 +0300

    Part 20. Connection SessionManager to ViewModel

--- app/src/main/java/com/example/dagger/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/example/dagger/ui/auth/AuthViewModel.java
@@ -8,6 +8,7 @@ import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
+import com.example.dagger.SessionManager;
 import com.example.dagger.models.User;
 import com.example.dagger.network.auth.AuthApi;
 
@@ -21,59 +22,54 @@ public class AuthViewModel extends ViewModel {
 
     private static final String TAG = "AuthViewModel";
 
-    private final AuthApi authApi;
-
-    private MediatorLiveData<AuthResource<User>> authUser = new MediatorLiveData<>();
+    // inject
+    private final SessionManager sessionManager; // @Singleton scoped dependency
+    private final AuthApi authApi; // @AuthScope scoped dependency
 
     @Inject
-    public AuthViewModel(AuthApi authApi) {
+    public AuthViewModel(AuthApi authApi, SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         this.authApi = authApi;
         Log.d(TAG, "AuthViewModel: viewmodel is working...");
-
     }
 
-    public void authenticateWithId(int userId){
-        authUser.setValue(AuthResource.loading((User)null));
-
-        final LiveData<AuthResource<User>> source = LiveDataReactiveStreams.fromPublisher(
-
-                authApi.getUser(userId)
-
-                        // instead of calling onError (error happens)
-                        .onErrorReturn(new Function<Throwable, User>() {
-                            @Override
-                            public User apply(Throwable throwable) throws Exception {
-                                User errorUser = new User();
-                                errorUser.setId(-1);
-                                return errorUser;
-                            }
-                        })
-
-                        .map(new Function<User, AuthResource<User>>() {
-                            @Override
-                            public AuthResource<User> apply(User user) throws Exception {
-                                if(user.getId() == -1){
-                                    return AuthResource.error("Could not authenticate", (User)null);
-                                }
-                                return AuthResource.authenticated(user);
-                            }
-                        })
-
-                        .subscribeOn(Schedulers.io())
-        );
-
-        authUser.addSource(source, new Observer<AuthResource<User>>() {
-            @Override
-            public void onChanged(AuthResource<User> user) {
-                authUser.setValue(user);
-                authUser.removeSource(source);
-            }
-        });
+    public LiveData<AuthResource<User>> observeAuthState(){
+        return sessionManager.getAuthUser();
     }
 
-    public LiveData<AuthResource<User>> observeUser(){
-        return authUser;
+    public void authenticateWithId(int userId) {
+        Log.d(TAG, "attemptLogin: attempting to login.");
+        sessionManager.authenticateWithId(queryUserId(userId));
     }
+
+    private LiveData<AuthResource<User>> queryUserId(int userId) {
+
+        return LiveDataReactiveStreams.fromPublisher(authApi.getUser(userId)
+
+                // instead of calling onError, do this
+                .onErrorReturn(new Function<Throwable, User>() {
+                    @Override
+                    public User apply(Throwable throwable) throws Exception {
+                        User errorUser = new User();
+                        errorUser.setId(-1);
+                        return errorUser;
+                    }
+                })
+
+                // wrap User object in AuthResource
+                .map(new Function<User, AuthResource<User>>() {
+                    @Override
+                    public AuthResource<User> apply(User user) throws Exception {
+                        if(user.getId() == -1){
+                            return AuthResource.error("Could not authenticate", null);
+                        }
+                        return AuthResource.authenticated(user);
+                    }
+                })
+                .subscribeOn(Schedulers.io()));
+    }
+
+
 }
 
 
