f4739ab8cf37ae9a52de2caf38084a5b77c9224c
Igor Novikov <novikov.mail@inbox.ru>
2020-12-19 16:44:56 +0300

    Part 26. Profile fragment works

--- app/src/main/java/com/example/dagger/ui/main/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/dagger/ui/main/profile/ProfileViewModel.java
@@ -4,16 +4,26 @@ import android.util.Log;
 
 import javax.inject.Inject;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.dagger.SessionManager;
+import com.example.dagger.models.User;
+import com.example.dagger.ui.auth.AuthResource;
 
 public class ProfileViewModel extends ViewModel {
 
-    private static final String TAG = "ProfileViewModule";
+    private static final String TAG = "DaggerExample";
+
+    private final SessionManager sessionManager;
 
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         Log.d(TAG, "ProfileViewModel: viewmodel is ready...");
     }
 
-
+    public LiveData<AuthResource<User>> getAuthenticatedUser(){
+        return sessionManager.getAuthUser();
+    }
 }
