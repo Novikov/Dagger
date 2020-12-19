2d532b956b6bf39b2311a43759016d8034922bdc
Igor Novikov <novikov.mail@inbox.ru>
2020-12-19 15:21:23 +0300

    Part 22,23 creation of toolbar menu in MainActivity

--- app/src/main/java/com/example/dagger/ui/main/MainActivity.java
+++ app/src/main/java/com/example/dagger/ui/main/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.dagger.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.dagger.BaseActivity;
@@ -16,7 +20,24 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout: {
+                sessionManager.logOut();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
